60fcd4e0bd801e728beb0fce20c58ba73e18ec46
githubnotunix <githubnotunix@github.com>
2019-11-06 06:03:44 -0600

    almost done error checking

--- src/client.java
+++ src/client.java
@@ -5,21 +5,91 @@ import java.util.Scanner;
 
 public class client {
 	public static void main(String args[]) throws IOException {
-	int number, balance, temp, withdraw, deposit;
+	int balance, temp;
+	int withdrawInt;
+	String number, withdraw, deposit;
 	Scanner sc = new Scanner(System.in);
-	Socket s = new Socket("10.35.3.25", 123);
+	Socket s = new Socket("127.0.0.1", 123);
 	Scanner sc1 = new Scanner(s.getInputStream());
 	System.out.println("Automated Teller Machine");
     System.out.println("Choose 1 for Withdraw");
     System.out.println("Choose 2 for Deposit");
     System.out.println("Choose 3 for Check Balance");
     System.out.println("Choose 4 for EXIT");
-	number = sc.nextInt();
+	
 	PrintStream p = new PrintStream(s.getOutputStream());
+	//p.println(number);
+	number = sc.nextLine();
 	p.println(number);
 	while(true)
 	{
-		if(number == 1 || number == 2 || number == 3) {
+		if(number.equals("1"))
+		{
+			
+			System.out.println("Enter an amount to withdraw");
+			
+			while(true) {
+				withdraw = sc.nextLine();
+			try {
+				withdrawInt = Integer.parseInt(withdraw);
+				break;
+			}
+			catch(NumberFormatException e) {
+				System.out.println("Type in a valid amount to withdraw");
+			}
+			}
+			p.println(withdraw);
+			balance = sc1.nextInt();
+			System.out.println("Current Balance: " + balance);
+			System.out.println("");
+			System.out.println("Automated Teller Machine");
+		    System.out.println("Choose 1 for Withdraw");
+		    System.out.println("Choose 2 for Deposit");
+		    System.out.println("Choose 3 for Check Balance");
+		    System.out.println("Choose 4 for EXIT");
+			number = sc.nextLine();
+			p.println(number);
+		}
+		else if(number.equals("2"))
+		{
+			System.out.println("Enter amount to deposit");
+			while(true) {
+				deposit = sc.nextLine();
+			try {
+				int depositInt = Integer.parseInt(deposit);
+				break;
+			}
+			catch(NumberFormatException e) {
+				System.out.println("Type in a valid amount to deposit");
+			}
+			}
+			p.println(deposit);
+			balance = sc1.nextInt();
+			System.out.println("Current Balance: " + balance);
+			System.out.println("");
+			System.out.println("Automated Teller Machine");
+		    System.out.println("Choose 1 for Withdraw");
+		    System.out.println("Choose 2 for Deposit");
+		    System.out.println("Choose 3 for Check Balance");
+		    System.out.println("Choose 4 for EXIT");
+			number = sc.nextLine();
+			p.println(number);
+		}
+		else if(number.equals("3"))
+		{
+			//System.out.println("Enter amount");
+			balance = sc1.nextInt();
+			System.out.println("Current Balance: " + balance);
+			System.out.println("");
+			System.out.println("Automated Teller Machine");
+		    System.out.println("Choose 1 for Withdraw");
+		    System.out.println("Choose 2 for Deposit");
+		    System.out.println("Choose 3 for Check Balance");
+		    System.out.println("Choose 4 for EXIT");
+			number = sc.nextLine();
+			p.println(number);
+		}
+		/*if(number == "1" || number == "2" || number == "3") {
 			balance = sc1.nextInt();
 			System.out.println("Current Balance " + balance);
 			System.out.println("");
@@ -28,20 +98,33 @@ public class client {
 		    System.out.println("Choose 2 for Deposit");
 		    System.out.println("Choose 3 for Check Balance");
 		    System.out.println("Choose 4 for EXIT");
-			number = sc.nextInt();
+			number = sc.nextLine();
 			p.println(number);
+		}*/
+		else if(number.contentEquals("4")) {
+			try {
+				
+				System.exit(0);
+			}
+			catch(Exception e){
+				System.out.println("Have a nice day");
+			}
 		}
-		else if(number != 1 || number != 2 || number != 3 || number != 4){
+		else{
             	System.out.println("That is not a valid option");
     			System.out.println("Automated Teller Machine");
     		    System.out.println("Choose 1 for Withdraw");
     		    System.out.println("Choose 2 for Deposit");
     		    System.out.println("Choose 3 for Check Balance");
     		    System.out.println("Choose 4 for EXIT");
-            	number = sc.nextInt();
+            	number = sc.nextLine();
             	p.println(number);
 		}
+		/*else {
+			
+		}*/
 			
 	}
+	//s.setKeepAlive(true);
 	}
 }
--- src/server.java
+++ src/server.java
@@ -6,20 +6,23 @@ import java.util.Scanner;
 
 public class server {
 	public static void main(String args[]) throws IOException {
-		int number, temp = 0, withdraw, deposit;
+		int temp = 0;
+		String withdraw, deposit;
+		String number;
 		int balance = 10000;
 		ServerSocket s1 = new ServerSocket(123);
 		Socket ss = s1.accept();
 		Scanner sc = new Scanner(ss.getInputStream());
 		Scanner scanner = new Scanner(System.in);
 		PrintStream p = new PrintStream(ss.getOutputStream());
-		
+
 		//number = sc.nextInt();
 		//temp = sc.nextInt();
-		 while(true)
-	        {
-	            number = sc.nextInt();
-	            /*if(number != 1 || number != 2 || number != 3 || number != 4) {
+		while(true)
+		{
+			number = sc.nextLine();
+			int parsedInt = Integer.parseInt(number);
+			/*if(number != 1 || number != 2 || number != 3 || number != 4) {
 	            	System.out.println("That is not a valid option");
 	    			System.out.println("Automated Teller Machine");
 	    		    System.out.println("Choose 1 for Withdraw");
@@ -28,44 +31,78 @@ public class server {
 	    		    System.out.println("Choose 4 for EXIT");
 	            	number = sc.nextInt();
 	            }*/
-	            switch(number)
-	            {
-	                case 1:
-	                System.out.print("Enter money to be withdrawn:");
-	                withdraw = scanner.nextInt();
-	                if(balance >= withdraw)
-	                {
-	                    balance = balance - withdraw;
-	                    //System.out.println("Please collect your money");
-	                    p.println(balance);
-	                }
-	                else
-	                {
-	                    System.out.println("Insufficient Balance");
-	                }
-	                System.out.println("");
-	                break;
-	 
-	                case 2:
-	                System.out.print("Enter money to be deposited:");
-	                deposit = scanner.nextInt();
-	                balance = balance + deposit;
-	                //System.out.println("Your Money has been successfully depsited");
-	                p.println(balance);
-	                System.out.println("");
-	                break;
-	 
-	                case 3:
-	                System.out.println("Balance : "/*+balance*/);
-	                p.println(balance);
-	                System.out.println("");
-	                break;
-	 
-	                case 4:
-	                System.out.println("Have a nice day!");
-	                System.exit(0);
-	            }
-	        }
+			switch(parsedInt)
+			{
+			case 1:
+				//System.out.print("Enter money to be withdrawn:");
+				/*while() {*/
+					//withdraw = scanner.nextInt();
+				
+					if(sc.hasNextLine()) {
+						
+						withdraw = sc.nextLine();
+						int withdrawInt = 0;
+						try {
+							withdrawInt = Integer.parseInt(withdraw);
+							//p.print(withdrawInt);
+						}
+						catch(Exception e) {
+							System.out.println("Error: " + e.toString());
+
+						}
+						/*finally
+						{
+							withdrawInt = 0;
+							System.out.println("withdraw canceled");
+						}*/
+						//p.print(withdrawInt);p
+						//p.println(balance);
+						if(balance >= withdrawInt)
+						{
+							
+							//withdraw = sc.nextLine();
+							balance = balance - withdrawInt;
+							//System.out.println("Please collect your money");
+							p.println(balance);
+							break;
+						}
+						else
+						{
+							//p.println(balance);
+							//System.out.println("Insufficient Balance");
+						}
+						System.out.println("");
+					}
+					else {
+						System.out.println("Enter a valid number");
+						//withdraw = scanner.nextInt();
+					}
+				//}
+
+				break;
+
+			case 2:
+				//System.out.print("Enter money to be deposited:");
+				deposit = sc.nextLine();
+				int depositInt = Integer.parseInt(deposit);
+				balance = balance + depositInt;
+				//System.out.println("Your Money has been successfully depsited");
+				p.println(balance);
+				System.out.println("");
+				break;
+
+			case 3:
+				//System.out.println("Balance : "/*+balance*/);
+				p.println(balance);
+				//System.out.println("");
+				break;
+
+			case 4:
+				//System.out.println("Have a nice day!");
+				//s1.setKeepAlive(true, 3000);
+				//System.exit(0);
+			}
+		}
 		/*if(number == 1) {
 			balance = balance + 0;
 		}
